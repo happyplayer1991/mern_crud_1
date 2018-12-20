7f29d36f5cbb3a75ff475afd196cb40172b9b6cb
freshcoderchan <freshcoderjack@outlook.com>
2018-12-20 07:56:09 -0800

    Create form

--- src/components/create.component.js
+++ src/components/create.component.js
@@ -5,9 +5,26 @@ import React, { Component } from 'react';
 export default class Create extends Component {
   render() {
     return (
-      <div>
-        <p>Welcome to Create Component!!</p>
-      </div>
+     <div style={{marginTop: 10}}>
+      <h3>Add New Business</h3>
+      <form>
+        <div className="form-group">
+          <label>Add Person Name: </label>
+          <input type="text" className="form-control" />
+        </div>
+        <div className="form-group">
+          <label>Add Business Name: </label>
+          <input type="text" className="form-control" />
+        </div>
+        <div className="form-group">
+          <label>Add GST Number: </label>
+          <input type="text" className="form-control" />
+        </div>
+        <div className="form-group">
+          <button type="submit" className="btn btn-primary">Register Business</button>
+        </div>
+      </form>
+     </div> 
     )
   }
 }
