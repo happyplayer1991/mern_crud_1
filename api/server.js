d59b97ea6b7aa354e20ca17a6d392cecd3a38fe1
freshcoderchan <freshcoderjack@outlook.com>
2018-12-20 14:54:23 -0800

    Add routes

--- api/server.js
+++ api/server.js
@@ -7,6 +7,7 @@ const PORT = 4000;
 const cors = require('cors');
 const mongoose = require('mongoose');
 const config = require('./config/DB');
+const businessRoute = require('./routes/business.route');
 
 mongoose.Promise = global.Promise;
 mongoose.connect(config.DB, { useNewUrlParser: true }).then(
@@ -17,6 +18,8 @@ mongoose.connect(config.DB, { useNewUrlParser: true }).then(
 app.use(cors());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+
+app.use('/business', businessRoute);
 
 app.listen(PORT, function() {
   console.log('Server is running on Port:', PORT);
