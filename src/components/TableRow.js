c0edb9067874e9a88cc63cf173f5db0303ce21d3
freshcoderchan <freshcoderjack@outlook.com>
2018-12-21 08:37:12 -0800

    Edit Feature

---
+++ src/components/TableRow.js
@@ -0,0 +1,30 @@
+// TableRow.js
+
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+class TableRow extends Component {
+  render() {
+    return (
+      <tr>
+        <td>
+          {this.props.obj.person_name};
+        </td>
+        <td>
+          {this.props.obj.business_name};
+        </td>
+        <td>
+          {this.props.obj.business_gst_number};
+        </td>
+        <td>
+          <Link to={"/edit/" + this.props.obj._id} className="btn btn-primary">Edit</Link>
+        </td>
+        <td>
+          <button className="btn btn-danger">Delete</button>
+        </td>
+      </tr>
+    );
+  }
+}
+
+export default TableRow;
